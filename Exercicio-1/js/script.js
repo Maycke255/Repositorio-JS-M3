3cbfdbb43c0c0133a91a967d1bc24f6dba271681
Maycke255 <reydsonk@gmail.com>
2025-04-01 23:24:38 -0300

    Enviando alterações

--- Exercicio-1/js/script.js
+++ Exercicio-1/js/script.js
@@ -14,3 +14,85 @@
 // - Após o jogador ser removido o campo de texto deve ser limpo.
 
 // Dica: lembrando que é possível acessar o texto de um input através da propriedade value.
+
+let jogadores = []
+
+function exibirJogadores() {
+    const section = document.getElementById(`section-pai`);
+    const lista = document.createElement(`ul`)
+
+    if (jogadores.length === 0){
+        section.innerText = `Nenhum Jogador adicionado`;
+        return;
+    }
+
+    jogadores.forEach(function (element){
+        const item = document.createElement(`li`)
+        item.textContent = `Nome: ${element.nome}, Posição: ${element.posicao}, Camisa: ${element.camisa}`
+
+        lista.appendChild(item);
+        section.appendChild(lista)
+    });
+
+}
+
+function addJogador () {
+    const nome = document.getElementById(`nome`).value;
+    const posicao = document.getElementById(`posicao`).value;
+    const camisa = document.getElementById(`camisa`).value;
+
+    if (nome === `` || posicao === `` || camisa === ``) {
+        alert(`Por favor, preencha os campos para adicionar um jogador`);
+        return;
+    }
+
+    let confirmPrompt = confirm(`Deseja adicionar escalar o jogador ${nome}, na posição ${posicao}, com a camisa ${camisa}?`);
+    
+    if (confirmPrompt) {
+        const jogadorConfirmado = {
+            nome,
+            posicao,
+            camisa
+        }
+
+        jogadores.push(jogadorConfirmado);
+
+        exibirJogadores();
+
+        document.getElementById(`nome`).value = ``;
+        document.getElementById(`posicao`).value = ``;
+        document.getElementById(`camisa`).value = ``;
+    } else {
+        alert(`Operação cancelada...`)
+        return;
+    }
+}
+
+function removerJogador() {
+    let removeInput = document.getElementById(`numero-camisa`).value;
+
+    if (removeInput === ``) {
+        alert(`Por favor, escale um jogador antes para poder remover`);
+        return;
+    };
+
+    let remove = jogadores.findIndex(function (element){
+        return element.camisa === removeInput
+    });
+
+    if (remove === -1) {
+        alert(`Número da camisa não encontrado.`);
+        return;
+    }
+
+    const nomeJogador = jogadores[remove].nome;
+
+    let removeConfirm = confirm(`Deseja remover o jogador ${nomeJogador} da camisa ${removeInput}?`);
+
+    if (removeConfirm) {
+        jogadores.splice(remove, 1);
+        exibirJogadores()
+
+        document.getElementById(`numero-camisa`).value = ``;
+    }
+}
