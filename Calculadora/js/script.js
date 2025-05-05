494239e9ae656a0c787cbc76824f89d88d272862
Maycke <reydsonk@gmail.com>
2025-05-05 11:09:09 -0300

    Enviando comentarios completos

--- Calculadora/js/script.js
+++ Calculadora/js/script.js
@@ -1,6 +1,6 @@
-const main = document.getElementById(`main`)
+const main = document.querySelector(`main`)
 // Pegando o root do CSS
-const root = document.getElementById(`:root`)
+const root = document.querySelector(`:root`)
 // Coletando o input desabilitado
 const resultInput = document.getElementById(`result`)
 // Coletando o ID do input onde irão aparecer os numeros e caracteres a serem ultilizados nas operacões
@@ -22,6 +22,12 @@ document.querySelectorAll(`.charKey`).forEach(function (charKeyBtns){
 document.getElementById(`clear`).addEventListener(`click`, function () {
     // Apenas colocamos uma string vazia no input e depois focamos o input, no caso apos ser limpado ele e selecionado
     input.value = ``
+
+    if (resultInput.value = `ERROR`) {
+        resultInput.value = ``
+        resultInput.classList.remove(`error`)
+    }
+
     input.focus()
 })
 
@@ -53,11 +59,55 @@ input.addEventListener(`keydown`, function (ev){
 document.getElementById(`equal`).addEventListener(`click`, calculate)
 
 function calculate() {
+    /* Definimos por padrão que ao clicar no calculate, o botão irá mostrar uma mensagem de ERROR, no caso se não tiver nada para ser executado no input, e mesmo assim o
+    usuario tente clicar em calcular, essa mensagem irá aparecer, ou então previnimos também o comportamento do eval de carregar algum codigo malicioso caso o usuario
+    digite algum, afinal, caso ele digite, vai dar um monte de erro no console, então com essa mensagem "ERROR", escrita dessa forma, o eval entende que aquilo e um erro,
+    então ele não executa o codigo. */
+    resultInput.value = `ERROR`
+    resultInput.classList.add(`error`)
+
     /* Essa e a parte de calcular, no caso iremos ultilizar a propriedade "eval", essa proprieade avalia as strings, numeros ou caracteres digitados pelo usuario,
     no caso ela transforma os caracteres digitados pelo usuario em codigos javaScript, ela roda os caracteres ainda por cima, essa propriedade e muito perigosa, e deve
     ser usada com muito cautela e cuidado, afinal, caso o usuario digite algum codigo malicioso dentro do input, vai estar valendo, por que essa propriedade irá avaliar
     o que o usuario digitou e ver se e valido como JS, caso seja ela vai rodar.
-    Em seguida, atribuimos o value do input desabilitado que fica embaixo ao result da operacão que o usuario fez */
+    Em seguida, atribuimos o value do input desabilitado que fica embaixo ao result da operacão que o usuario fez.
+    Por fim, removemos também a class do error, caso contrario vai ficar vermelhão */
     const result = eval(input.value)
     resultInput.value = result
+    resultInput.classList.remove(`error`)
 }
+
+/* Função para copiar o valor presente no input de resultado, e simples, primeiro pegamos a ação do botão, no caso o currentTarget, que e responsavel por armazenar o evento
+do botão ao ser clicado, caso seja clicado a verificação irá mudar o texto para Copied e adicionar uma classe para destacar o botão, sem seguida o mais importante, usamos uma
+função propria, uma função nativa já implementada no navegador, a clipboard, depois adicionamos um "writeText", que significa escrever texto, o que queremos escrever? O value
+do resultInput, então escrevemos o value para a area de transferencia, depois ao clicar novamente, voltamos o texto Copy e retiramos a class. */
+document.getElementById(`copyToClipboard`).addEventListener(`click`, function (ev) {
+    const button = ev.currentTarget
+    
+    if (button.innerText === `Copy`) {
+        button.innerText = `Copied!`
+        button.classList.add(`success`)
+        navigator.clipboard.writeText(resultInput.value)
+    } else {
+        button.innerText = `Copy`
+        button.classList.remove(`success`)
+    }
+})
+
+/* Por fim, acessamos o botão de trocar de tema, definimos uma verificação para o main da pagina, no caso a pagina esteja no tema dark, ela vai trocar pro tema light
+e virse e evrsa */
+document.getElementById(`themeSwitcher`).addEventListener(`click`, function () {
+    if (main.dataset.theme === `dark`) {
+        root.style.setProperty(`--bg-color`, `#f1f5f9`)
+        root.style.setProperty(`--border-color`, `#aaa`)
+        root.style.setProperty(`--font-color`, `#212529`)
+        root.style.setProperty(`--primary-color`, `#26834a`)
+        main.dataset.theme = `light`
+    } else {
+        root.style.setProperty(`--bg-color`, `#212529`)
+        root.style.setProperty(`--border-color`, `#666`)
+        root.style.setProperty(`--font-color`, `#f1f5f9`)
+        root.style.setProperty(`--primary-color`, `#4dff91`)
+        main.dataset.theme = `dark`
+    }
+})
